d745e4f3f5bb03523753d24103d02e691c5823cf
Pavlo <106017048+99bruno@users.noreply.github.com>
2024-05-12 13:52:39 +0300

    test

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const presentationBtn = document.getElementById('presentationBtn');
 const codeBtn = document.getElementById('codeBtn');
 const content = document.getElementById('content');
+const backBtn = document.getElementById('backBtn');
 
 presentationBtn.addEventListener('click', function() {
     content.innerHTML = '<iframe src="Presentation_1.html" class="fullscreen" frameborder="0"></iframe>';
@@ -9,3 +10,9 @@ presentationBtn.addEventListener('click', function() {
 codeBtn.addEventListener('click', function() {
     content.innerHTML = '<pre><code>// Ваш код тут</code></pre>';
 });
+
+
+
+if (window.innerWidth <= 768) { // Якщо ширина екрану менше або рівна 768px (тобто, мобільний пристрій)
+    backBtn.style.display = 'block';
+}
