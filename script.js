6721653eaf4c0fce6eeacb060a639028977c4d35
Pavlo <106017048+99bruno@users.noreply.github.com>
2024-05-12 13:47:58 +0300

    test

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ const codeBtn = document.getElementById('codeBtn');
 const content = document.getElementById('content');
 
 presentationBtn.addEventListener('click', function() {
-    // Замість "presentation.html" вставте шлях до вашої HTML-презентації
-    content.innerHTML = '<iframe src="Presentation_1.html" width="100%" height="100%" frameborder="0" allowfullscreen></iframe>';
+    content.innerHTML = '<iframe src="Presentation_1.html" class="fullscreen" frameborder="0"></iframe>';
 });
 
 codeBtn.addEventListener('click', function() {
-    // Тут можна вставити ваш код або інший вміст
     content.innerHTML = '<pre><code>// Ваш код тут</code></pre>';
 });
