4d73691915f84487dc0ef120e7939ca2be3900b5
Pavlo <106017048+99bruno@users.noreply.github.com>
2024-05-14 16:36:38 +0300

    main

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 
+// Необов'язковий JavaScript
+document.querySelectorAll('.team-member').forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        item.querySelector('.member-info').style.transform = 'translateY(0)';
+        item.querySelector('img').style.transform = 'translateY(-20%)';
+    });
+
+    item.addEventListener('mouseleave', () => {
+        item.querySelector('.member-info').style.transform = 'translateY(100%)';
+        item.querySelector('img').style.transform = 'translateY(0)';
+    });
+});
 
 
