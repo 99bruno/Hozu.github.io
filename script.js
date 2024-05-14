73bb33df80c38bed177b76169673fd610ed6646b
Pavlo <106017048+99bruno@users.noreply.github.com>
2024-05-14 17:25:16 +0300

    main

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ document.querySelectorAll('.team-member').forEach(item => {
 });
 
 
+// Ініціалізація слайдера
+const priceSlider = document.getElementById('price-slider');
+const priceFrom = document.getElementById('price-from');
+const priceTo = document.getElementById('price-to');
+
+noUiSlider.create(priceSlider, {
+    start: [0, 1000], // Початкове значення слайдера
+    connect: true, // З'єднувати позначки на слайдері
+    range: { // Діапазон значень слайдера
+        'min': 0,
+        'max': 1000
+    },
+    // Встановлення параметрів вигляду слайдера
+    pips: {
+        mode: 'values',
+        values: [0, 1000],
+        density: 4
+    }
+});
+
+// Отримання значень "від" і "до" при зміні слайдера
+priceSlider.noUiSlider.on('update', function (values) {
+    const from = parseInt(values[0]);
+    const to = parseInt(values[1]);
+
+    // Оновлення значень "від" і "до" на сторінці
+    priceFrom.textContent = from;
+    priceTo.textContent = to;
+});
+
+
+
